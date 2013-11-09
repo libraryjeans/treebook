6655b804f15a9eb9068f9d7fca3b5b7d5b62272c
Laura Robinson <laurajeans@gmail.com>
2013-11-09 15:25:07 -0800

    started from scratch

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Treebook::Application.config.secret_token = '33556d6265e114f2a48ffc4d185dd9e27b33c4a43fb007bf48f0fbb1c7c2bc41b457aa0e95bd82449e5f85b5f6798690acc9c10e5eb512409cbb670d82d47fc5'
+Treebook::Application.config.secret_token = '3b3a0b076340a9d8959592e7f44e91606c0e805c2ffdbf1581bf0b893cb60d6c25685a368f1f69daf92351bc7360461e3fe17d065f673631f72c076dcaee2b04'
